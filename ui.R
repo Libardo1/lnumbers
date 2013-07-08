3fdc2b8b950ce7fad03c2799401aa17073d9e8b6
tgouhier <tarik.gouhier@gmail.com>
2013-07-08 08:13:59 -0400

    Add URL of code

--- ui.R
+++ ui.R
@@ -10,9 +10,10 @@ shinyUI(pageWithSidebar(
                 "Maximum number of samples:", 
                 value = 1000,
                 min = 600, 
-                max = 10000)
-    ),
-    
+                max = 10000),
+  br(),
+  helpText(a(href="https://github.com/tgouhier/lnumbers", target="_blank", "View code"))),
+        
   mainPanel(
     plotOutput("plot", height="900px")
   )
